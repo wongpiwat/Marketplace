3827fe0b516b3ad3231636eb65b6960a0a06670a
minsuphawit <suphawit.kh@ku.th>
2018-04-18 15:29:53 +0700

    comment image

--- database/migrations/2018_04_18_074521_create_markets_table.php
+++ database/migrations/2018_04_18_074521_create_markets_table.php
@@ -16,18 +16,15 @@ class CreateMarketsTable extends Migration {
             $table->string('date');
             $table->text('details')->nullable();
             $table->string('teaser')->nullable();
-            $table->unsignedInteger('images');
             $table->unsignedInteger('created_by');
             $table->timestamps();
-            $table->foreign('images')->references('id')->on('market_images');
             $table->foreign('created_by')->references('id')->on('users');
-        });
+        }
     }
 
     public function down() {
         Schema::enableForeignKeyConstraints();
         Schema::table('markets', function (Blueprint $table) {
-            $table->dropForeign(['images']);
             $table->dropForeign(['created_by']);
         });
         Schema::disableForeignKeyConstraints();
