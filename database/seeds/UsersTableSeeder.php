319c667f507d393b12426b2afb630c7cbc48d7d8
minsuphawit <suphawit.kh@ku.th>
2018-04-18 15:51:58 +0700

    oi

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,14 +5,14 @@ use Illuminate\Database\Seeder;
 class UsersTableSeeder extends Seeder {
 
     public function run() {
-        $admin = new App\User;
-        $admin->username = 'admin';
-        $admin->password = bcrypt('adminpassword');
-        $admin->name = 'Administrator';
-        $admin->email = 'admin@example.com';
-        $admin->access_level = 'administrator';
-        $admin->is_enabled = true;
-        $admin->save();
-        $users = factory(App\User::class,50)->create();
+        // $admin = new App\User;
+        // $admin->username = 'admin';
+        // $admin->password = bcrypt('adminpassword');
+        // $admin->name = 'Administrator';
+        // $admin->email = 'admin@example.com';
+        // $admin->access_level = 'administrator';
+        // $admin->is_enabled = true;
+        // $admin->save();
+        $users = factory(App\User::class,10)->create();
     }
 }
