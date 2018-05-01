ab66b7ba6898c1c3d41aeda762835107e22c2cbb
wongpiwat <wongpiwat153@hotmail.com>
2018-05-01 17:26:17 +0700

    Add files

--- database/seeds/MarketImagesTableSeeder.php
+++ database/seeds/MarketImagesTableSeeder.php
@@ -15,19 +15,13 @@ class MarketImagesTableSeeder extends Seeder
 
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_m11.jpg';
+      $market_img->path ='ss_3.jpg';
       $market_img->type='screenshot';
       $market_img->save();
 
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_m12.jpg';
-      $market_img->type='screenshot';
-      $market_img->save();
-
-      $market_img = new App\MarketImage;
-      $market_img->market_id = $market;
-      $market_img->path ='lo_layout1.jpg';
+      $market_img->path ='lo_layout.jpg';
       $market_img->type='layout';
       $market_img->save();
 
@@ -35,22 +29,22 @@ class MarketImagesTableSeeder extends Seeder
       $market = App\Market::where('id', '2')->value('id');
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='lo_layout2.jpg';
+      $market_img->path ='lo_layout.jpg';
       $market_img->type='layout';
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_rat.jpg';
+      $market_img->path ='ss_1.jpg';
       $market_img->type='screenshot';
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_poom.jpg';
+      $market_img->path ='ss_2.jpg';
       $market_img->type='screenshot';
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_nut.jpg';
+      $market_img->path ='ss_3.jpg';
       $market_img->type='screenshot';
       $market_img->save();
 
@@ -73,7 +67,7 @@ class MarketImagesTableSeeder extends Seeder
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_กก.jpg';
+      $market_img->path ='ss_m33.jpg';
       $market_img->type='screenshot';
       $market_img->save();
 
@@ -89,6 +83,12 @@ class MarketImagesTableSeeder extends Seeder
       $market_img->path ='ss_1.jpg';
       $market_img->type='screenshot';
       $market_img->save();
+      $market_img = new App\MarketImage;
+      $market_img->market_id = $market;
+      $market_img->path ='ss_nn.jpg';
+      $market_img->type='screenshot';
+      $market_img->save();
+
 
       $market = App\Market::where('id', '5')->value('id');
       $market_img = new App\MarketImage;
@@ -106,17 +106,17 @@ class MarketImagesTableSeeder extends Seeder
 
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_3พพ.jpg';
+      $market_img->path ='ss_31.jpg';
       $market_img->type='screenshot';
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='ss_3ออ.jpg';
+      $market_img->path ='ss_32.jpg';
       $market_img->type='screenshot';
       $market_img->save();
       $market_img = new App\MarketImage;
       $market_img->market_id = $market;
-      $market_img->path ='lo_layoutฟ.jpg';
+      $market_img->path ='lo_layout.jpg';
       $market_img->type='layout';
       $market_img->save();
 
